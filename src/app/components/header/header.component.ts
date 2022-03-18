2d70a9050fd1c6118864b81ac28cbe3f07d9444c
Amynnct <amei.nguyennct@gmail.com>
2022-03-17 23:20:31 -0400

    update with route, and add_task form

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
-  constructor() { }
+  showAddTask: boolean = true;
+  subscription!: Subscription; 
+
+  constructor(private uiServeice: UiService, private router:Router) { 
+    this.subscription = this.uiServeice.onToggle().subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() {
-    console.log('toggle');
+    //this.showAddTask = !this.showAddTask;
+    this.uiServeice.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url == route;
   }
 }
